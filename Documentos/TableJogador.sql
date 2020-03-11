299a7e872be6adefb2a740e94ee3060db95a71d0
MARCOS PAULO MARTINS DE OLIVEIRA <marcos.pmoliveira1@senacsp.edu.br>
2020-03-11 15:06:35 -0300

    Tabela Jogador SQL

---
+++ Documentos/TableJogador.sql
@@ -0,0 +1,16 @@
+USE admquiz;
+
+CREATE TABLE jogador
+(
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome varchar (255) NOT NULL,
+    cpf float (255) NOT NULL UNIQUE,
+    telefone int (255) NOT NULL,
+    telefone2 int (255),
+    aluno varchar(255) NOT NULL,
+    curso varchar(255) NOT NULL,
+)
+
+INSERT INTO jogador (nome, cpf, telefone, telefone2, aluno, curso) VALUES ("Marcos", 46491294862, 14998609309, "Sim", "Técnico em Informática");
+INSERT INTO jogador (nome, cpf, telefone, telefone2, aluno, curso) VALUES ("Luiz", 11122233344, 14996328661, "Sim", "Técnico em Informática");
+INSERT INTO jogador (nome, cpf, telefone, telefone2, aluno, curso) VALUES ("Ezequiel", 55566677788, 14996037848, "Sim", "Técnico em Informática");
